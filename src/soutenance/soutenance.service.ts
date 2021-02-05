bb8dd0d9d4b90beb9a118f8979d642c588f1b6be
MissaouiSyrine <syrine.missaoui@insat.u-carthage.tn>
2021-02-05 14:39:18 +0100

    Soutenance API

--- src/soutenance/soutenance.service.ts
+++ src/soutenance/soutenance.service.ts
@@ -2,7 +2,7 @@ import {Injectable, NotFoundException} from '@nestjs/common';
 import {InjectRepository} from "@nestjs/typeorm";
 import {SoutenanceModel} from "./model/soutenance.model";
 import {Repository} from "typeorm";
-import { ObjectID } from 'mongodb';
+
 import {UserModel} from "../user/models/user.model";
 import {SubjectModel} from "../subject/models/subject.model";
 import {AjoutSoutenanceDto} from "./dto/AjoutSoutenance.dto";
@@ -24,11 +24,39 @@ export class SoutenanceService {
     ) {}
 
     async findAllSoutenances(): Promise<SoutenanceModel[]> {
-        return await this.SoutenanceRepository.find();
+        const full_soutenances = [];
+        const soutenances  = await this.SoutenanceRepository.find();
+        console.log('la liste des soutenances ', soutenances)
+        for (var i = 0; i < soutenances.length; i++) {
+            if (!soutenances[i])
+                throw new NotFoundException(`La soutenance n'est pas disponible`);
+
+             console.log('la soutenance ${i}',soutenances[i])
+            const subject = await this.SubjectRepository.findOne(soutenances[i].sujet);
+            if(!subject)
+                throw new NotFoundException(`sujet inexistant`);
+            const nom_sujet = subject.title;
+            const student = await this.UserRepository.findOne(subject.student);
+            const nom_etudiant = student.lastname;
+            const prenom_etudiant =student.firstname;
+            const responsable_Insat = await this.UserRepository.findOne(subject.teacher);
+            const responsableInsat = responsable_Insat.lastname + ' ' + responsable_Insat.firstname;
+            const examinateur = await this.UserRepository.findOne(soutenances[i].examinateur);
+            const nom_examinateur = examinateur.lastname + ' ' + examinateur.firstname;
+            const presidentJury =  await this.UserRepository.findOne(soutenances[i].presidentJury);
+            const nom_presidentJury = presidentJury.lastname + ' ' + presidentJury.firstname;
+            full_soutenances.push({...soutenances[i],nom_sujet,nom_etudiant,prenom_etudiant,responsableInsat
+                ,nom_examinateur,nom_presidentJury,subject,student,responsable_Insat,examinateur,presidentJury})
+        }
+            /*return await this.SoutenanceRepository.find();*/
+            return full_soutenances;
     }
 
     async findSoutenanceById(id) {
         const soutenance = await this.SoutenanceRepository.findOne(id);
+        if (!soutenance) {
+            throw new NotFoundException(`La soutenance n'est pas disponible`);
+        }
         if (soutenance) {
             const sujet = await this.SubjectRepository.findOne(soutenance.sujet);
             const presidentJury = await this.UserRepository.findOne(soutenance.presidentJury);
@@ -37,9 +65,7 @@ export class SoutenanceService {
             const etudiant =await this.UserRepository.findOne(sujet.student);
             const full_object = { ...soutenance,sujet, presidentJury, examinateur,responsable_INSAT, etudiant};
             return full_object;
-            if (!soutenance) {
-                throw new NotFoundException(`La soutenance n'est pas disponible`);
-            }
+
         }
     }
     async findSoutenanceByStudentNumber(insNumber: number) {
