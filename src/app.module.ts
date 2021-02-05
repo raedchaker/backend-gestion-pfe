70a06dae62a6c57105458285640a61cebebd2910
altafallahabbassi@gmail.com <altafallahabbassi@gmail.com>
2021-02-05 20:12:14 +0100

    file upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,11 @@ import * as dotenv from 'dotenv';
 import { UserModule } from './user/user.module';
 import { UserModel } from './user/models/user.model';
 import { AuthModule } from './auth/auth.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { SendMailService } from './generics/send-mail/send-mail.service';
+import { FileUploadService } from './generics/file-upload/file-upload.service';
+
 
 dotenv.config();
 
@@ -20,9 +25,31 @@ dotenv.config();
       synchronize: true,
     }),
     AuthModule,
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: process.env.EMAIL_ID, // generated ethereal user
+          pass: process.env.EMAIL_PASS // generated ethereal password
+        },
+      },
+      defaults: {
+        from: '"nest-modules" <ne5demwn5adem@outlook.fr>', // outgoing email ID
+      },
+      template: {
+        dir: process.cwd() + '/template/',
+        adapter: new HandlebarsAdapter(), // or new PugAdapter()
+        options: {
+          strict: true,
+        },
+      },
+    }),
+
     
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, FileUploadService ,],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
+
 const corsOptions = {
   origin: ['http://localhost:4201', 'http://localhost:4200'],
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
@@ -9,6 +11,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors(corsOptions);
+  app.use(bodyParser.json({limit: '50mb'}));
+  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
   await app.listen(3000);
 }
 bootstrap();
